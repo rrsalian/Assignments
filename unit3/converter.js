898901e884d9c4ce8d9b99fdb1ba97917582160b
rrsalian7@gmail.com <rrsalian7@gmail.com>
2023-01-26 12:59:24 -0500

    updated converter.js

--- unit3/converter.js
+++ unit3/converter.js
@@ -6,14 +6,15 @@ let unit = temperature.charAt(temperature.length - 1);
 let temp = temperature.substring(0,temperature.length - 1);
 let tempConvert = 0;
 
-if (unit === "F") {
+if ((unit === "F") || (unit === "f")) {
     tempConvert = (temp - 32) * 5/9 ;
+    console.log(`Temperature ${temperature} in Celcius is ${tempConvert}C`);
 }
-else if (unit === "C") {
+else if ((unit === "C") || (unit === "c")) {
     tempConvert = temp * 1.8 + 32;
+    console.log(`Temperature ${temperature} in Farenhiet is ${tempConvert}F`);
 }
 else {
     console.log("Invalid Unit of temperature");
 }
 
-console.log(tempConvert);
