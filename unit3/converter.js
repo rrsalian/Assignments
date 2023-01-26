102efaeb4506742635a5cfac4d8b231dc268be9b
rrsalian7@gmail.com <rrsalian7@gmail.com>
2023-01-26 12:40:52 -0500

    updated converter.js

--- unit3/converter.js
+++ unit3/converter.js
@@ -4,17 +4,13 @@ const temperature = prompt("What is the Temperature?");
 
 let unit = temperature.charAt(temperature.length - 1);
 let temp = temperature.substring(0,temperature.length - 1);
-console.log(unit);
-console.log(temp);
-let tempArray = temperature.split(unit);
-console.log(tempArray);
 let tempConvert = 0;
 
 if (unit === "F") {
     tempConvert = (temp - 32) * 5/9 ;
 }
 else if (unit === "C") {
-    tempConvert = temp;
+    tempConvert = temp * 1.8 + 32;
 }
 else {
     console.log("Invalid Unit of temperature");
