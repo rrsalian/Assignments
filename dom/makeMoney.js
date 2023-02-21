d53dd64d56efe90e66c8e394c6ef5479bde5df42
Rajesh Salian <rajeshsalian@rocketmortgage.com>
2023-02-20 19:47:02 -0500

    Updated makeMoney.js

--- dom/makeMoney.js
+++ dom/makeMoney.js
@@ -1,18 +1,25 @@
-(function() {
+(function () {
 
-    const howMany   = document.getElementById('how-many');    
-    const whichCoin = document.getElementById('which-coin');
-    const makeMoney = document.getElementById('make-money');
-    
-    makeMoney.addEventListener('click', (event) => {
-        event.preventDefault();
-        
-        const coinContainer = document.getElementById("coinContainer");
-        const newCoinP = document.createElement("p");        
-        newCoinP.classList.add("showCoin");
-        newCoinP.textContent = whichCoin.value;
-        
-        coinContainer.append(newCoinP);
-    });
+  const howMany = document.getElementById('how-many');
+  const whichCoin = document.getElementById('which-coin');
+  const makeMoney = document.getElementById('make-money');
+  const coinContainer = document.getElementById("coinContainer");
+
+  makeMoney.addEventListener('click', (event) => {    
+    event.preventDefault();    
+    for (let i = 0; i < howMany.value; i++) {
+      const newCoinP = document.createElement("p");
+      newCoinP.classList.add("showCoin");
+      newCoinP.textContent = whichCoin.value;
+      coinContainer.append(newCoinP);
+
+      newCoinP.addEventListener('click',(e) => {
+        console.log(e.target);
+        e.target.remove();
+      })
+    }
+  });
+
+
 
 }())
