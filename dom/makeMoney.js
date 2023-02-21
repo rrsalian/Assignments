2a9dc07b1f23c822c2c3ae25b969dc725ed29bf6
Rajesh Salian <rajeshsalian@rocketmortgage.com>
2023-02-20 19:04:14 -0500

    makeMoney Initial draft

--- dom/makeMoney.js
+++ dom/makeMoney.js
@@ -1,34 +1,18 @@
 (function() {
 
-    const howMany = document.getElementById('how-many');    
+    const howMany   = document.getElementById('how-many');    
     const whichCoin = document.getElementById('which-coin');
     const makeMoney = document.getElementById('make-money');
     
-    
     makeMoney.addEventListener('click', (event) => {
         event.preventDefault();
-        console.log(howMany.value);    
-        console.log(whichCoin.value);
         
+        const coinContainer = document.getElementById("coinContainer");
+        const newCoinP = document.createElement("p");        
+        newCoinP.classList.add("showCoin");
+        newCoinP.textContent = whichCoin.value;
+        
+        coinContainer.append(newCoinP);
     });
 
 }())
-
-/*
-const newCoin = document.createElement("div");
-      const newCoinP = document.createElement("p");
-      if (coinType.value === "halfDollar") {
-        newCoinP.innerText = "Half Dollar";
-      } else {
-        newCoinP.innerText = coinType.value;
-      }
-
-      newCoin.append(newCoinP);
-      newCoin.addEventListener("click", (event) => {
-        console.log(event.target);
-        // event.target.remove();
-      });
-      newCoin.classList.add("coin");
-      newCoin.classList.add(coinType.value);
-      coinContainer.append(newCoin);
-*/
