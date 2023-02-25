25ae836d30e0b91f27b18a7e1262299a43b176f5
Rajesh Salian <rajeshsalian@rocketmortgage.com>
2023-02-24 20:11:42 -0500

    menuButton assignment

--- dom/menuButton.js
+++ dom/menuButton.js
@@ -1,22 +1,28 @@
 (function () {
     const hamburger = document.querySelector(".hamburger");
     const navMenu = document.querySelector(".nav-menu");
+    const content1 = document.getElementById('content1');
+    const content2 = document.getElementById('content2');
 
     hamburger.addEventListener("click", () => {
-        hamburger.classList.toggle("active");        
+        hamburger.classList.toggle("active");
         navMenu.classList.toggle("active");
     })
 
     const navLinks = document.querySelectorAll(".nav-link");
-   
-    for(let link of navLinks) {
-        link.addEventListener('click',(e) => {
-            const mainContainer = document.getElementById('mainContainer');
-            const mainContent = document.getElementById('mainContent');
-            
-            console.log(link.innerText);
+
+    for (let link of navLinks) {
+        link.addEventListener('click', (e) => {
+            if (link.innerText.toLowerCase() === 'letters') {
+                content1.innerText = 'A B C D E F G H I J K L M';
+                content2.innerText = 'N O P Q R S T U V W X Y Z';
+            }
+            else {
+                content1.innerText = '1 2 3 4 5 6 7 8 9 10';
+                content2.innerText = '';
+            }
             navMenu.classList.remove("active");
             hamburger.classList.remove("active");
         })
-    }    
+    }
 }());
